3fcd14aabfa8ec86eadaaaed53d5aed38a914ca0
Sophia Mantegari <sophiamantegari@gmail.com>
2025-06-19 09:59:33 -0400

    lol

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,12 +4,11 @@
     return (
       <section 
       id="footer" 
-      className="flex flex-col justify-center snap-start h-[20%] w-full relative bg-slate-800">
-        <div className="relative  items-center left-[75%]">
-
-        <div className=" p-5 text-white font-medium uppercase text-base " style={{ fontFamily: 'Lora, san-serif' }}>
-            <p>sophiamantegari@gmail.com</p>
-        </div>
+      className="flex flex-col justify-center  snap-start h-[20%] w-full relative bg-slate-800 ">
+        <div className=" items-center text-right ">
+            <div className="p-5  text-white font-medium uppercase text-base " style={{ fontFamily: 'Lora, san-serif' }}>
+                <p>sophiamantegari@gmail.com</p>
+            </div>
         </div>
       </section>
     );
